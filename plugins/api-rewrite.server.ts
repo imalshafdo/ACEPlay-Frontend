8354e65d14e39788f45d219c68d9bc2976261720
Imalsha PCB <imalshaportcitybpo@gmail.com>
2026-07-05 07:36:53 +0000

    update backend aceplay-admin-backend-production-1c28.up.railway.appapi

--- plugins/api-rewrite.server.ts
+++ plugins/api-rewrite.server.ts
@@ -5,10 +5,10 @@ export default defineNuxtPlugin(() => {
   const originalFetch = globalThis.$fetch
   if (originalFetch) {
     globalThis.$fetch = (request, opts) => {
-      if (typeof request === 'string' && request.startsWith('http://localhost:5000')) {
+      if (typeof request === 'string' && request.startsWith('aceplay-admin-backend-production-1c28.up.railway.appapi')) {
         // Rewrite localhost:5000 requests to the docker backend container name
-        const internalUrl = process.env.BACKEND_INTERNAL_URL || 'http://backend:5000'
-        request = request.replace('http://localhost:5000', internalUrl)
+        const internalUrl = process.env.BACKEND_INTERNAL_URL || 'aceplay-admin-backend-production-1c28.up.railway.appapi'
+        request = request.replace('aceplay-admin-backend-production-1c28.up.railway.appapi', internalUrl)
       }
       return originalFetch(request, opts)
     }
